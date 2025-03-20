339698c29bc30dabf6238c363b2a265772a72eae
bridgefisher <guy.duke2@gmail.com>
2025-03-20 13:57:06 -0600

    cicd with workflows

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yo dude! How's it going? No really...</p>
+        <p>Yo dude! How's it going? No really...AWS?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
